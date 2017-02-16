64b43064eef1f30b8e7f2dd4a5057d641e984e07
Benyu Wan <benyuwan@gmail.com>
2017-02-16 16:19:05 +0800

    Padding data to topic page

--- routes/topic.js
+++ routes/topic.js
@@ -3,7 +3,16 @@ var superagent = require('superagent');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('topic');
+    var id = req.query.id;
+    superagent.get("https://cnodejs.org/api/v1/topic/" + id)
+        .end(function(err, sres) {
+            var result = JSON.parse(sres.text);
+            var data = result.data;
+            console.log(data.replies);
+            res.render('topic', {
+                data: data
+            });
+        });
 });
 
 module.exports = router;
