ac63d58a4650ab9c36c32cf8acdafab8cf3f51f7
Benyu Wan <benyuwan@gmail.com>
2017-02-17 17:12:02 +0800

    Setting css style for topic page

--- routes/topic.js
+++ routes/topic.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var superagent = require('superagent');
+var getTimeInfo = require('../utils/gettimeinfo.js');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     var id = req.query.id;
     superagent.get("https://cnodejs.org/api/v1/topic/" + id)
-        .end(function(err, sres) {
+        .end(function (err, sres) {
             var result = JSON.parse(sres.text);
             var data = result.data;
-            console.log(data.replies);
+            data.create_at = getTimeInfo(data.create_at);
+
+            if (data.top == true) {
+                data.category = '置顶';
+            } else if (data.good == true) {
+                data.category = '精华';
+            }
+
+            if (data.tab == 'ask') {
+                data.tab = '问答';
+            } else if (data.tab == 'share') {
+                data.tab = '分享';
+            } else {
+                data.tab = '招聘';
+            }
+
+            for( i in data.replies) {
+                data.replies[i].create_at = getTimeInfo(data.replies[i].create_at); 
+            }
             res.render('topic', {
                 data: data
             });
